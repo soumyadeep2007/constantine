7699b5222f4e73f680c22bc8b551253d17244a8a
tusharm58 <tushar.mohandas@stonybrook.edu>
2017-10-15 17:06:21 -0400

    Master back merge

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
 def read_config():
     config = dict()
+    with open('config.txt', 'r') as f:
+        for line in f:
+            if line[0] != '#':
+                (key, sep, val) = line.partition('=')
+                # if the line does not contain '=', it is invalid and hence ignored
+                if len(sep) != 0:
+                    val = val.strip()
+                    config[key.strip()] = int(val) if str.isdecimal(val) else val
+                print(config)
     with open('config.csv', 'r') as f:
         for line in f:
             if line[0] != '#':
