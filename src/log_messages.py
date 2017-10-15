1fb91f73954fb01fb8c67921d8c183fae1d34869
Soumyadeep Chakraborty <soumyadeep.chakraborty@stonybrook.edu>
2017-10-15 12:52:56 -0400

    Corrected yield points and message reception and sending

--- src/log_messages.py
+++ src/log_messages.py
@@ -4,8 +4,13 @@ INVALID_SIGNATURE = 'A signature mismatch occurred from source:'
 # Client messages
 REQUESTING_CONFIG = 'Config requested from Olympus by client='
 CONFIG_REQUEST_TIMEOUT = 'Config request timed out...retrying...'
-CONFIG_FETCHED = 'The config was fetched from Olympus'
+CONFIG_FETCHED = 'The config was fetched from Olympus:'
+OPERATION_CREATED = 'The following operation was created:'
+REQUEST_MADE = 'A request for an operation was made'
 RESULT_RECEIVED = 'A result was received from replica='
+OPERATION_RESULT_PAIR = 'The operation result pair is='
+REQUEST_TIMEOUT = 'Wait for result timed out for operation='
+RETRANSMISSION = 'Retransmitting request to all replicas for operation='
 
 # Head messages
 RESULT_CACHED = 'Result was cached, returning result from cache to='
