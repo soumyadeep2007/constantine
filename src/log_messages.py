2ce2ca06625b98e6554e7a8cfd4e5339562641ef
tusharm58 <tushar.mohandas@stonybrook.edu>
2017-10-23 10:28:49 -0400

    Update log messages

--- src/log_messages.py
+++ src/log_messages.py
@@ -19,19 +19,21 @@ VALID_RESULT_RECEIVED = 'The result received is valid:(result, operation)='
 INVALID_RESULT_RECEIVED = 'The result received is valid:(result, operation)='
 OPERATION_RESULT_PAIR = 'The operation result pair is='
 REQUEST_TIMEOUT = 'Wait for result timed out for operation='
-RETRANSMISSION = 'Retransmitting request to all _replicas for operation='
+RETRANSMISSION = 'Retransmitting request to all replicas for operation='
 INVALID_REQUEST_SOURCE = 'Request received from invalid source'
 ERROR_MESSAGE_RECEIVED = 'An error message was received from replica='
 
-
 # Replica messages
+SENDING_ERROR_MESSAGE = 'Sending error message (client_id, replica_id)'
 INVALID_SLOT = 'Shuttle received with invalid request slot'
-INVALID_ORDER_PROOF_REPLICA_MISSING = 'Invalid order proof because order statement missing for replica '
-INVALID_ORDER_PROOF_REPLICA_SIGNATURE_MISMATCH = 'Invalid order proof because order statement signature mismatch for replica '
-INVALID_ORDER_PROOF_REPLICA_SLOT_MISMATCH = 'Invalid order proof because operation mismatch for replica '
+INVALID_ORDER_PROOF_REPLICA_MISSING = 'Invalid order proof because order statement missing for replica (replica_id, self._id)'
+INVALID_ORDER_PROOF_REPLICA_SIGNATURE_MISMATCH = 'Invalid order proof because order statement signature mismatch for replica (replica_id, self._id)'
+INVALID_ORDER_PROOF_REPLICA_SLOT_MISMATCH = 'Invalid order proof because operation mismatch for replica (replica_id, self._id)'
+INVALID_RESULT_PROOF_REPLICA_MISSING = 'Invalid result proof because order statement missing for replica (replica_id, self._id)'
+INVALID_RESULT_PROOF_REPLICA_SIGNATURE_MISMATCH = 'Invalid result proof because result statement signature mismatch for replica (replica_id, self._id)'
+INVALID_RESULT_PROOF_REPLICA_HASH_MISMATCH = 'Invalid result proof because hash mismatch for replica (replica_id, self._id)'
 
 # Head messages
 RESULT_CACHED = 'Result was cached, returning result from cache to='
 RESULT_PENDING = 'Awaiting result shuttle from downstream _replicas'
 RESULT_SENT_AFTER_WAIT = 'Result received at head, now forwarding to client='
-
